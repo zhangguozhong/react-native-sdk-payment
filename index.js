df0acc3e0dae540ce3dd3efabfe75adcd36a3fb2
zhangguozhong <guozhong.zhang@mobilemd.cn>
2019-03-11 18:46:46 +0800

    PaymentSdk

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { NativeModules } from 'react-native';
 const { RNPayment } = NativeModules;
 
-export default class UtilsPayment {
+export default class PaymentSdk {
 
     static onPay(type,orderInfo,appScheme,successCallback,failureCallback) {
         RNPayment.onPay(type,orderInfo,appScheme,successCallback,failureCallback);
